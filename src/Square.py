52fcfb2407ecf785fc13c7f1332675410e7a016d
artem <maffiasng@gmail.com>
2025-12-12 23:13:17 +0300

    Скорее всего я сделал все не так, и бо не совсем разобрался в теме ООП. Что то мне уже сотсарта курса не посебе.

---
+++ src/Square.py
@@ -0,0 +1,10 @@
+from Rectangle import *
+
+class Square(Rectangle):
+    def __init__(self, side_a: int | float):
+        if side_a <= 0:
+            raise ValueError("Ну, а квадрат чем тебе не угадил, какой  еще ноль ? ")
+        super().__init__(side_a,side_a)
+        self.side_a = side_a
+
+
